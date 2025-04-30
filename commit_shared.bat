d2907ad8e9606ee8d393002c58dfe5792df80e37
ddoing11 <kjeg9580@naver.com>
2025-04-30 20:13:03 +0900

    백업 (클릭 -> 음성 출력)

--- commit_shared.bat
+++ commit_shared.bat
@@ -1,15 +1,5 @@
 ﻿@echo off
-echo [공유용] shared/ 폴더만 GitHub에 푸시합니다.
-
-REM shared 폴더만 Git에 추가
-git add shared/
-git add .gitignore
-git add README.md
-
-REM 커밋 메시지 입력받기
-set /p msg=공유용 커밋 메시지를 입력하세요:
-
-git commit -m "%msg%"
-git push
-
-pause
+git checkout shared
+git add .
+git commit -m "공유용 업데이트"
+git push origin shared
